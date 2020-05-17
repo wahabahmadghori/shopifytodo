b15512cc093303107fbee20f650bfaff9df8db87
wahabahmadghori <wahabahmadghori@gmail.com>
2020-05-17 19:47:46 +0500

    initial commit

---
+++ src/components/input-todo/TodoInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoInput extends Component {
+    render() {
+        return (
+            <article>
+                <h2>Hello from TodoInput</h2>
+            </article>
+        )
+    }
+}
---
+++ src/components/list-todo/TodoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import TodoItem from '../item-todo/TodoItem'
+
+export default class TodoList extends Component {
+    render() {
+        return (
+            <article>
+                <h2>Hello from TodoList</h2>
+                <TodoItem/>
+            </article>
+        )
+    }
+}
